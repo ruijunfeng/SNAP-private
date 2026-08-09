bdcca394e1c445a660bfcb7988a70d837f465707
ruijunfeng <fengruijuan558@gmail.com>
2026-08-09 21:46:08 +0800

    Refactor prediction threshold checks for consistency in ablation study and performance analysis scripts

--- research_questions/ablation_study.py
+++ research_questions/ablation_study.py
@@ -16,35 +16,35 @@ results = []
 
 # w/o SNAP
 snap_results = load_json("results/calm/version_0/predictions.json")
-y_pred = torch.tensor([int(snap_results[str(i)]["y_proba"] >=0.5) for i in data_module.test_indices])
+y_pred = torch.tensor([int(snap_results[str(i)]["y_proba"]>=0.5) for i in data_module.test_indices])
 y_proba = torch.tensor([snap_results[str(i)]["y_proba"] for i in data_module.test_indices])
 result = calculate_classification_metrics(y_true, y_pred, y_proba)
 results.append(("w/o SNAP", result))
 
 # SNAP w/o numerical embedding
 snap_results = load_json("results/snap/without_numerical_embedding/version_0/predictions.json")
-y_pred = torch.tensor([int(snap_results[str(i)]["y_proba"] >=0.5) for i in data_module.test_indices])
+y_pred = torch.tensor([int(snap_results[str(i)]["y_proba"]>=0.5) for i in data_module.test_indices])
 y_proba = torch.tensor([snap_results[str(i)]["y_proba"] for i in data_module.test_indices])
 result = calculate_classification_metrics(y_true, y_pred, y_proba)
 results.append(("SNAP w/o numerical embedding", result))
 
 # SNAP w/o numerical profiling
 snap_results = load_json("results/snap/without_numerical_profiling/version_0/predictions.json")
-y_pred = torch.tensor([int(snap_results[str(i)]["y_proba"] >=0.5) for i in data_module.test_indices])
+y_pred = torch.tensor([int(snap_results[str(i)]["y_proba"]>=0.5) for i in data_module.test_indices])
 y_proba = torch.tensor([snap_results[str(i)]["y_proba"] for i in data_module.test_indices])
 result = calculate_classification_metrics(y_true, y_pred, y_proba)
 results.append(("SNAP w/o numerical profiling", result))
 
 # SNAP w/o projector
 snap_results = load_json("results/snap/without_projector/version_0/predictions.json")
-y_pred = torch.tensor([int(snap_results[str(i)]["y_proba"] >=0.5) for i in data_module.test_indices])
+y_pred = torch.tensor([int(snap_results[str(i)]["y_proba"]>=0.5) for i in data_module.test_indices])
 y_proba = torch.tensor([snap_results[str(i)]["y_proba"] for i in data_module.test_indices])
 result = calculate_classification_metrics(y_true, y_pred, y_proba)
 results.append(("SNAP w/o projector", result))
 
 # SNAP
 snap_results = load_json("results/snap/full_model/version_0/predictions.json")
-y_pred = torch.tensor([int(snap_results[str(i)]["y_proba"] >=0.5) for i in data_module.test_indices])
+y_pred = torch.tensor([int(snap_results[str(i)]["y_proba"]>=0.5) for i in data_module.test_indices])
 y_proba = torch.tensor([snap_results[str(i)]["y_proba"] for i in data_module.test_indices])
 result = calculate_classification_metrics(y_true, y_pred, y_proba)
 results.append(("SNAP", result))
--- research_questions/performance_analysis.py
+++ research_questions/performance_analysis.py
@@ -33,16 +33,16 @@ y_proba = torch.tensor([informed_gpt_results[str(i)]["y_proba"] for i in data_mo
 result = calculate_classification_metrics(y_true, y_pred, y_proba)
 results.append(("Informed GPT", result))
 
-# CALM (LoRA) 4 6 8 15 16 20 21
+# CALM (LoRA)
 lora_results = load_json("results/calm/version_0/predictions.json")
-y_pred = torch.tensor([int(lora_results[str(i)]["y_proba"] >=0.5) for i in data_module.test_indices])
+y_pred = torch.tensor([int(lora_results[str(i)]["y_proba"]>=0.5) for i in data_module.test_indices])
 y_proba = torch.tensor([lora_results[str(i)]["y_proba"] for i in data_module.test_indices])
 result = calculate_classification_metrics(y_true, y_pred, y_proba)
 results.append(("CALM", result))
 
-# SNAP 2 3 7 10 11 16 20 21 19
+# SNAP
 snap_results = load_json("results/snap/full_model/version_0/predictions.json")
-y_pred = torch.tensor([int(snap_results[str(i)]["y_proba"] >=0.5) for i in data_module.test_indices])
+y_pred = torch.tensor([int(snap_results[str(i)]["y_proba"]>=0.5) for i in data_module.test_indices])
 y_proba = torch.tensor([snap_results[str(i)]["y_proba"] for i in data_module.test_indices])
 result = calculate_classification_metrics(y_true, y_pred, y_proba)
 results.append(("SNAP", result))
